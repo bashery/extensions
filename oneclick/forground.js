ce257ccf05ef8634a45af7b55e9b3542dda89de2
fedor <ahmedalbachiry@gmail.com>
2021-02-25 11:20:31 +0300

    somting

--- oneclick/forground.js
+++ oneclick/forground.js
@@ -1,2 +1,4 @@
-consol.log("hello from forground")
+//consol.log("hello from forground")
 chrome.tabs.executescript(null, {file: "./forground"}, ()=> console.log("injecte a malware"))
+//chrome.console.log("test coc extenstion")
+
