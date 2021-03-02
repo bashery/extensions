92f1dd247db0eede46bcc3f2667cc417d7e79c3e
fedor <ahmedalbachiry@gmail.com>
2021-03-02 19:47:36 +0300

    improve css

--- hamzabot/popup.js
+++ hamzabot/popup.js
@@ -1,17 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("button").addEventListener("click", onclick, false)
-    function onclick() {
-        chrome.tabs.query({currentWindow:true, active:true}, 
-            function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, 'hello !', setCount)
-            })
-    } 
-    function setCount(res) {
 
-        let div = document.createElement("div")
-        div.textContent = `${res.count} coins`
-        document.body.appendChild(div)
-    }
-})
+
+document.querySelector("button").addEventListener("click", onclick, false)
+function onclick() {
+    chrome.tabs.query({currentWindow:true, active:true}, 
+        function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, 'hello !', show(tabs))
+        })
+} 
+function show(tabs) {
+    alert("all tabs is "+ tabs.length)
+    console.log(typeof(tabs))
+}
+function setCount(res) {
+    let div = document.createElement("div")
+    div.textContent = `${res.count} coins`
+    document.body.appendChild(div)
+}
+
+
+
+//document.addEventListener("DOMContentLoaded", function() {
+//})
 
 
