fd51b883950d7ef5791090cd34cd718e8ab3df4e
fedor <ahmedalbachiry@gmail.com>
2021-03-07 23:51:32 +0300

    fix injection bug & add some files

--- click/content.js
+++ click/content.js
@@ -1,3 +1,14 @@
-console.log("content console injected riyht now")
-console.log(window.location.hostname)
+console.log("content injected riyht now into ", window.location.hostname)
+
+const seter = document.createElement('button')
+seter.innerText = 'set data'
+seter.id = 'seter'
+document.querySelector('body').appendChild(seter)
+
+
+const geter = document.createElement('button')
+geter.innerText = 'get data'
+geter.id = 'geter'
+document.querySelector('body').appendChild(geter)
+
 
