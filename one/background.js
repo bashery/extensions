cc64565615bc6a1c835fa74dc677b59695f7ccfd
fedor <ahmedalbachiry@gmail.com>
2021-04-20 16:10:12 +0300

    open close orders

--- one/background.js
+++ one/background.js
@@ -1,3 +1,5 @@
+
+let defPrice =  5
 let basicUrl = "https://www.binance.com/en/trade/ONE_USDT?layout=basic"
 let fututerUrl = "https://www.binance.com/en/futures/ONEUSDT_perpetual" 
 
@@ -37,30 +39,29 @@ chrome.tabs.query({active: false, currentWindow: true}, function(tabs) {
 
 });
 
-let defPrice = 5 
 var spotMsg  = 0
 var futureMsg = 0
 var on = false
 var start = 0
 var deal = "no"
+var asset = 9999999999.0
+var msgasset = 9999999999.0
 
 chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+
+   
+    // all is already ?
+    start + isStart(msg.startMsg)
+    on = isOn(start)
     
-    if (msg.startMsg) {
-        console.log("starting bot", msg.startMsg)
-        start++ 
-    } else if (msg.startMsg === false){
-        console.log("stoping bot", msg.startMsg)
-        start--
+    // get assets and parse it
+    msgasset = parseFloat(msg.asset)
+    if (msgasset < asset) {
+        asset = parseFloat(msg.asset)
     }
+    //console.log("On : ", on, "asset : ", asset)
     
 
-    if (start === 2) {
-        on = true
-    } else {
-        on = false
-    }
-    
     // get price from spicify tap
     if (sender.url === fututerUrl) {
         futureMsg = msg.message
@@ -70,15 +71,18 @@ chrome.runtime.onMessage.addListener(function(msg, sender, response) {
         spotMsg = msg.message
     }
 
+
     // order to by or sell if prices is much
     if ((futureMsg/1000) * defPrice <= futureMsg-spotMsg && deal !== "opened" && on) { // TODO i not sure her > or < ??
         deal = "opened"
         console.log( "OCASIO, open at : spot:", spotMsg, "future:", futureMsg)
+        console.log("ssset: ", asset)
         
+
         // send message to all tabs we work with
         for (let i = 0; i< mytabs.length; i++ ) {
             // TODO handle responde calback
-            chrome.tabs.sendMessage(mytabs[i], {message: "open deal"},  function(response ) {
+            chrome.tabs.sendMessage(mytabs[i], {message: "open", asset:asset},  function(response ) {
                 console.log(response) // done
             } );
 
@@ -88,9 +92,10 @@ chrome.runtime.onMessage.addListener(function(msg, sender, response) {
 
     // close and earn deal order if ocasion:
     if (spotMsg >= futureMsg && deal === "opened") {
+        // TODO parse and send assets
         for (let i = 0; i< mytabs.length; i++ ) {
             // TODO handle responde calback
-            chrome.tabs.sendMessage(mytabs[i], {message: "close deal"}, function(response) { 
+            chrome.tabs.sendMessage(mytabs[i], {message: "close"}, function(response) { 
                 console.log(response) 
             });
         }
@@ -102,10 +107,28 @@ chrome.runtime.onMessage.addListener(function(msg, sender, response) {
     if (deal === "opened") {
         console.log("opened")    
     } else {
-        console.log("not yet")    
+        console.log("no deal")    
     }
 
     response("no ocasion")
 })
 
+// helper function
 
+function isOn(start) {
+    if (start === 2) {
+        return true
+    }
+        return false
+}
+
+function isStart(startMessage) {
+    if (startMessage) {
+        console.log("starting bot", startMessage)
+        return 1
+    } else if (startMessage === false){
+        console.log("stoping bot",startMessage )
+        return -1
+    }
+ 
+}
--- one/content.js
+++ one/content.js
@@ -1,5 +1,4 @@
 // control buttons
-
 let btn = document.createElement('button')
 
 btn.innerText = " تشغيل "
@@ -23,66 +22,113 @@ btn.onclick = function() {
 
 
 sleeptime = 100 
-plus = 0
 
 if (!window.location.href.endsWith("perpetual")) {
-    plus = 3
+    sleeptime = 101
 }
 
 let price = document.querySelector('.showPrice')
 let prc = 0.0
+var asset
 var bot = false
-let inc = 0
+
+//let inc = 0
+
 price.onclick = function() {
-    inc ++
 
-        prc = parseFloat(price.innerText.replace(",", ""))
-        chrome.runtime.sendMessage({message:prc})
+    //inc ++
+    prc = parseFloat(price.innerText.replace(",", ""))
+    if (window.location.href.endsWith("perpetual")) {
+        asset = document.querySelector('#__APP > div > div > div > div.css-e4zj7m > div.css-s4sfla > div:nth-child(1) > div.css-s9gj39 > div.css-w8rmc6 > span').innerText
+    } else {
+        asset = document.querySelector('#__APP > div > div > div.css-e4zj7m > div > div.css-1cjqw9n > div.css-17gt9am > div.css-vurnku > div:nth-child(1) > div > div.css-yvo0m7 > span').innerText
+    }
 
+    chrome.runtime.sendMessage({message:prc, asset:asset})
+    
+    /*
     if (inc === 10) {
         console.log(prc)
         inc = 0
     }
+    */
+    
     setTimeout(function(){
        price.onclick()
-    }, sleeptime+plus)
+    }, sleeptime)
 }  
  
 price.onclick()
 
 
 
-
-
 chrome.runtime.onMessage.addListener((msg, sender, resp) => {
-    console.log(msg.message)
+    console.log("order: ", msg.message)
+    console.log("asset: ", msg.asset)
     
 
-    if (msg.message === "open deal") {
+    if (msg.message === "open") {
         if (window.location.href.endsWith('perpetual')) {
             // sell in futuer marcket
-            document.querySelector('button.css-y7ysid').click()
+            total = document.querySelector("#FormRow-normal-quantity")
+            total.value = msg.asset
+            console.log("opening deal with: ", msg.asset )
+
+            // marcket is a button to sell
+            btnMarcket = document.querySelector('#__APP > div > div > div > div.css-e4zj7m > div.css-s4sfla > div:nth-child(1) > div.css-na6azx > span.css-191wvd7').click()
+            btnMarcket.click()
+
+            // open deal by sell order
+            let btnBuy = document.querySelector('#__APP > div > div > div > div.css-e4zj7m > div.css-s4sfla > div:nth-child(1) > form > div.css-f4auaq > button.css-15rehdy')
+            btnBuy.click()
+
+            //document.querySelector('button.css-y7ysid').click()
+            // TODO procidure all contract staps
         }
 
         if (!window.location.href.endsWith('perpetual')) {
             // buy in spot marcket
-            document.querySelector('#orderformBuyBtn').click()
+            let total = document.querySelector('#FormRow-BUY-total')
+            total.value = msg.asset
+            console.log("opening deal with: ", msg.asset )
+
+            let marcket = document.querySelector('#__APP > div > div > div.css-e4zj7m > div > div.css-1cjqw9n > div.css-17gt9am > div.css-vurnku > div:nth-child(1) > div > div.css-8smnea > span.css-191wvd7')
+            marcket.click()
+            //document.querySelector('#orderformBuyBtn').click()
         }
 
     }
      
-    if (msg.message === "close deal") {
+    if (msg.message === "close") {
         if (window.location.href.endsWith('perpetual')) {
-            // buy in futuer marcket
-            document.querySelector('button.css-1ftari0').click()
-        } 
-        if (window.location.href.endsWith('basic')){
-            // sell
-            document.querySelector('#orderformSellBtn').click()
-        }
+            // close in futuer marcket (buy)
+            let closeFutur = document.querySelector('#__APP > div > div > div > div.react-grid-layout.layout > div:nth-child(2) > div > div > div > div.css-im6ko3 > div > div > div > div > div.list-container.css-wr25is > div.list-auto-sizer > div > div > div > div > div.closePosition > div > button')
+            closeFutur.click()
+            console.log("perpetual deal is closed")
 
+        } 
+        
+
+        if (!window.location.href.endsWith('perpetual')){
+            // sell
+            let marcket = document.querySelector('#__APP > div > div > div.css-e4zj7m > div > div.css-1cjqw9n > div.css-17gt9am > div.css-vurnku > div:nth-child(1) > div > div.css-8smnea > span.css-191wvd7')
+            marcket.click()
+            
+            // definde asset to to sell them
+            let assetElement = document.querySelector('#__APP > div > div > div.css-e4zj7m > div > div.css-1cjqw9n > div.css-17gt9am > div.css-vurnku > div:nth-child(2) > div > div.css-yvo0m7 > span')
+            sellAsset = parseFloat(assetElement.innerText)
+
+            // fill ammont element by size of asset thit need sell
+            let ammontElement = document.querySelector('#FormRow-SELL-quantity')
+            ammontElement.value = sellAsset
+            
+            // click sell button
+            let sellBtn = document.querySelector('#orderformSellBtn')
+            sellBtn.click()
+            console.log("spot deal is closed")
+
+        }
     }
-    
 
     resp("done") 
 })
